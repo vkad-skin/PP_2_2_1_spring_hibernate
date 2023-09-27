149cf41ae80dd70306d2178e6c79f3e44a9f7003
Vladislav <vladikul98@gmail.com>
2023-09-28 00:33:07 +0300

    1 version

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,49 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    public User findUserByCar(Car car) {
+        String hql = "from User as user where user.car.model=:carModel and user.car.series=:carSeries";
+
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql)
+                .setParameter("carModel", car.getModel())
+                .setParameter("carSeries", car.getSeries());
+
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            User user = new User();
+            user.setCar(new Car());
+            return user;
+        }
+
+    }
 }
